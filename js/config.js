9e28189722358c0c1d9dc6e9353bac22af7e845e
letian yi <letian yi>
2016-08-18 21:46:56 +0800

    add webim, elk, kafka page

--- js/config.js
+++ js/config.js
@@ -5,36 +5,61 @@ var PAGE_TYPE_LINK = PAGE_TYPE++;
 var ToolboxConfig = {
 	disc:"工具箱",
 	tools:{
+		/*
+		empty:{
+			type: PAGE_TYPE_SUBPAGE,
+			host:"",
+			page:"/empty.html",
+			desc:"空白页"
+		},
+		*/
 		muclog:{
 			type: PAGE_TYPE_SUBPAGE,
-			host: "http://d.easemob.com:3010",
-			page: "muclog.html",
+			rest: "http://d.easemob.com:3010",
+			page: "/muclog.html",
 			desc: "MUC日志查询",
 			active: true
 		},
 		msglog:{
 			type: PAGE_TYPE_SUBPAGE,
-			host: "http://123.56.92.91:1717/message",
-			page: "msglog.html",
+			rest: "http://localhost:1234",
+			page: "/msglog.html",
 			desc: "消息记录查询"
 		},
 		cipage:{
 			type: PAGE_TYPE_LINK,
-			host: "",
 			page: "http://ci-ali.easemob.com/",
 			desc: "CI监控"
 		},
 		zabbix:{
 			type: PAGE_TYPE_SUBPAGE,
-			host: "",
-			page: "http://123.56.207.23/zabbix",
+			page: "/zabbix.html",
 			desc: "zabbix监控"
 		},
-		empty:{
+		health:{
 			type: PAGE_TYPE_SUBPAGE,
-			host:"",
-			page:"empty.html",
-			desc:"空白页"
+			page:"http://123.57.141.188:52580/",
+			desc:"健康检查"
+		},
+		webim:{
+			type: PAGE_TYPE_SUBPAGE,
+			page: "/webim.html",
+			desc: "webim"
+		},
+		kafka:{
+			type: PAGE_TYPE_SUBPAGE,
+			page:"http://182.92.99.106/",
+			desc:"kafka监控"
+		},
+		grafana:{
+			type: PAGE_TYPE_SUBPAGE,
+			page:"http://grafana.op.easemob.com:10000/grafana/#/dashboard/db/easemob",
+			desc:"grafana"
+		},
+		elk:{
+			type: PAGE_TYPE_SUBPAGE,
+			page: "http://54.67.55.145/app/kibana",
+			desc: "ELK"
 		}
 	}
 }
